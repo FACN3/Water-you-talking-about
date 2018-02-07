41915ac38c0cad188f37e800a7528027c7eca51a
Amir Khoury <amirkhourykh@gmail.com>
2018-02-07 15:33:32 +0200

    Styled the Nav bar to fit mobile and desktop. Reference #16

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import styled from 'styled-components';
 import NavBar from './NavBar';
@@ -9,16 +10,28 @@ const Container = styled.div`
   align-items: center;
 `;
 const Title = styled.h1`
+  display: flex;
+  text-align: center;
   color: blue;
   font-size: 44px;
   font-weight: bold;
+
+  @media only screen and (max-width: 700px) {
+    font-size: 30px;
+  }
+
+  @media only screen and (max-width: 400px) {
+    font-size: 22px;
+  }
 `;
 
 const App = () => (
-  <Container>
-    <NavBar />
-    <Title>Water You Talking About?</Title>
-  </Container>
+  <BrowserRouter>
+    <Container>
+      <NavBar />
+      <Title>Water You Talking About?</Title>
+    </Container>
+  </BrowserRouter>
 );
 
 export default App;
