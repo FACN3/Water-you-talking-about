445c7171298d902beb819acd962663153d18dd7d
SamiSha99 <sami.shakkour@hotmail.com>
2018-02-11 15:58:46 +0200

    changes back to original files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { Switch, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
+import styled from 'styled-components';
+import NavBar from './NavBar';
+
+// Styling section
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background-color: #383854;
+`;
 
 // our App
 
 const App = () => (
   <BrowserRouter>
-    <Switch />
+    <Container>
+      <NavBar />
+    </Container>
   </BrowserRouter>
 );
 
