abf6da89f04cb36028cfc5d08ca627149fda6b2c
Amir Khoury <amirkhourykh@gmail.com>
2018-02-11 14:58:26 +0200

    [FIX] the navbar didnt show and the page didnt load. Reference #17

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import React from 'react';
 import Home from './Home';
 import NavBar from './NavBar';
+import './styles/reset';
 
 // our App
 
 const App = () => (
   <BrowserRouter>
-    <NavBar />
-    <Switch>
-      <Route exact path="/" component={Home} />
-    </Switch>
+    <Container>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Container>
   </BrowserRouter>
 );
 
