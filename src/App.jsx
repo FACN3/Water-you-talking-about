52a3f48755b01bef79985977737622da6c92883b
SamiSha99 <sami.shakkour@hotmail.com>
2018-02-11 14:57:07 +0200

    Removed files that have nothing to do with the PR

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import { Switch, BrowserRouter } from 'react-router-dom';
 import React from 'react';
-import Home from './Home';
-import NavBar from './NavBar';
 
 // our App
 
 const App = () => (
   <BrowserRouter>
-    <NavBar />
-    <Switch>
-      <Route exact path="/" component={Home} />
-    </Switch>
+    <Switch />
   </BrowserRouter>
 );
 
