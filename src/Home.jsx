1db8e0a37d9abe99031d6459a2826678e56a9ace
Amir Khoury <amirkhourykh@gmail.com>
2018-02-11 16:07:31 +0200

    Added title and some info content with styled.component. Reference #17

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  color: #383854;
+  font-family: Righteous;
+  margin: 1rem 0 0 0;
+  font-size: 2rem;
+`;
+
+const Info = styled.h1`
+  font-size: 2rem;
+  max-width: 40rem;
+
+  @media only screen and (max-width: 700px) {
+    font-size: 1rem;
+    width: 20rem;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 4rem;
+  margin: 0 0 1rem 0;
+  @media only screen and (max-width: 700px) {
+    font-size: 2rem;
+    margin: 0 0 0.5rem 0;
+  }
+`;
 
 const Home = () => (
-  <div>
-    <h1>This is Hello</h1>
-  </div>
+  <Container>
+    <Title>Welcome</Title>
+    <Info>Please select your daily activity and choose the amout by clicking on it again</Info>
+  </Container>
 );
 
 export default Home;
--- src/styles/reset.js
+++ src/styles/reset.js
@@ -3,6 +3,11 @@
 import { injectGlobal } from 'styled-components';
 
 injectGlobal`
+
+html {
+  background-color: #d1f4ff;
+}
+
 html,
 body,
 div,
