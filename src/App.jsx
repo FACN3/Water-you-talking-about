d1280381a1a51e2f147f5ae115fb655811aefd30
SamiSha99 <sami.shakkour@hotmail.com>
2018-02-11 15:53:02 +0200

    removes files from this PR

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-import { BrowserRouter } from 'react-router-dom';
+import { Switch, BrowserRouter } from 'react-router-dom';
 import React from 'react';
-import styled from 'styled-components';
-import NavBar from './NavBar';
-
-// Styling section
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  background-color: #383854;
-`;
 
 // our App
 
 const App = () => (
   <BrowserRouter>
-    <Container>
-      <NavBar />
-    </Container>
+    <Switch />
   </BrowserRouter>
 );
 
