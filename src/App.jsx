2abb261912e0b5020d5fdced9ff5a6f0eb08744f
SamiSha99 <sami.shakkour@hotmail.com>
2018-02-11 13:25:28 +0200

    Added a home page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
 import React from 'react';
 import Home from './Home';
-import Calculator from './Calculator';
-import Prices from './Prices';
-import Statistics from './Statistics';
 import NavBar from './NavBar';
 
 // our App
@@ -13,9 +10,6 @@ const App = () => (
     <NavBar />
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route path="/calculator" component={Calculator} />
-      <Route path="/prices" component={Prices} />
-      <Route path="/statistics" component={Statistics} />
     </Switch>
   </BrowserRouter>
 );
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Home extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello this is home</h1>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div>
+    <h1>This is Hello</h1>
+  </div>
+);
 
 export default Home;
