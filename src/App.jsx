0e5a8dd9d0b7fda785bdc6aa13f93d3167838230
SamiSha99 <sami.shakkour@hotmail.com>
2018-02-11 13:02:32 +0200

    Fixed some issues with App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import { Switch, BrowserRouter } from 'react-router-dom';
 import React from 'react';
-import Home from './Home';
-import Calculator from './Calculator';
-import Prices from './Prices';
-import Statistics from './Statistics';
 
 // our App
 
 const App = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/calculator" component={Calculator} />
-      <Route path="/prices" component={Prices} />
-      <Route path="/statistics" component={Statistics} />
-    </Switch>
+    <Switch />
   </BrowserRouter>
 );
 
