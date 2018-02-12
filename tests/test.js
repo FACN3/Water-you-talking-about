5e4080a12c7a406e68b88596fd317392224ef399
SamiSha99 <sami.shakkour@hotmail.com>
2018-02-12 15:24:04 +0200

    Fixed the requested changes

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,6 @@
 import test from 'tape';
 
-// var test = require('tape');
-
-test('Testing Travis', (t) => {
+test('1 = 1 ?', (t) => {
   const actual = 1;
   const expected = 1;
   t.equal(actual, expected, 'Should return 1 when given 1');
