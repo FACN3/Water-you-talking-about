4d989bbb9fbc9b394435162c8c546e7421ae46b7
Amir Khoury <amirkhourykh@gmail.com>
2018-02-12 19:31:55 +0200

    'Added a desktop version calculator page. Reference #7'

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -14,25 +14,39 @@ const Container = styled.section`
 const SubTitle = styled.h2`
   font-size: 1rem;
   color: #383854;
-  border-radius: 1rem;
-  margin: 2rem 0 1rem 0;
-  padding: 0.5rem 0;
+`;
+
+const Submit = styled.section`
+  font-size: 1rem;
+  max-width: 50%;
+  background-color: rgba(0, 0, 0, 0.25);
+  border: 0.05rem rgba(0, 0, 0, 0.5) solid;
+  padding: 0.5rem 7.5rem;
+  border-radius: 0.5rem;
 `;
 
 const Content = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   font-size: 1rem;
   color: #383854;
+  max-width: 50%;
   border-radius: 1rem;
-  margin: 2rem 0 1rem 0;
-  padding: 0.5rem 0;
+`;
+
+const IconContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  margin: 1rem 0 0 0;
 `;
 
 const Icon = styled.div`
-  font-size: 1rem;
-  color: #383854;
   border-radius: 1rem;
-  margin: 2rem 0 1rem 0;
-  padding: 0.5rem 0;
+  color: white;
+  background-color: #383854;
+  margin: 1rem 1rem;
+  padding: 1rem 2rem 5rem 2rem;
 `;
 
 const Calculator = () => (
@@ -41,9 +55,29 @@ const Calculator = () => (
       Please select your daily activity and choose the amout by clicking on it again
     </SubTitle>
     <Content>
-      <Icon>
-        <i className="fa fa-bath" />
-      </Icon>
+      <IconContainer>
+        <Icon>
+          <i className="fa fa-bath" />
+        </Icon>
+        <Icon>
+          <i className="fa fa-bath" />
+        </Icon>
+        <Icon>
+          <i className="fa fa-bath" />
+        </Icon>
+      </IconContainer>
+      <IconContainer>
+        <Icon>
+          <i className="fa fa-bath" />
+        </Icon>
+        <Icon>
+          <i className="fa fa-bath" />
+        </Icon>
+        <Icon>
+          <i className="fa fa-bath" />
+        </Icon>
+      </IconContainer>
+      <Submit>Calculate</Submit>
     </Content>
   </Container>
 );
