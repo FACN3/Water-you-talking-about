051ad15e1313da4b95dca95fa8e3509b27449458
SamiSha99 <sami.shakkour@hotmail.com>
2018-02-12 18:07:21 +0200

    Added a working calculator in react

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,23 +1,18 @@
-import data from './../fixtures/data';
-
 // userChoice is how many activities have been clicked
 
-function calculator(userChoice) {
-  const output = {
-    liters: 0,
-    cost: 0,
-  };
-
-  Object.keys(userChoice).forEach((choice) => {
-    data.activities.forEach((activity) => {
-      if (activity.name === choice) {
-        output.liters += activity.liters * userChoice[choice];
-        output.cost += activity.cost * userChoice[choice];
-      }
-    });
-  });
-
-  return output;
+export default function calculator(data, userChoice) {
+  return Object.keys(userChoice).reduce(
+    (acc, cur) => {
+      data.activities.forEach((activity) => {
+        if (activity.name === cur) {
+          const liters = activity.liters * userChoice[cur];
+          acc.liters += liters;
+          const cost = activity.cost * userChoice[cur];
+          acc.cost += cost;
+        }
+      });
+      return acc;
+    },
+    { liters: 0, cost: 0 },
+  );
 }
-
-export default calculator;
--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,57 @@
 import test from 'tape';
+import data from '../src/fixtures/data';
+import calculator from '../src/lib/calculator';
 
-test('1 = 1 ?', (t) => {
-  const actual = 1;
-  const expected = 1;
-  t.equal(actual, expected, 'Should return 1 when given 1');
+test('Test #0', (t) => {
+  const expected = {
+    liters: 9,
+    cost: 0.0801,
+  };
+
+  const actual = calculator(data, { toilet: 2 });
+  console.log(expected, actual);
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test('Test #1', (t) => {
+  const expected = {
+    liters: 56,
+    cost: 0.4984,
+  };
+
+  const actual = calculator(data, { shower: 7 });
+  console.log(expected, actual);
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test('Test #2', (t) => {
+  const expected = {
+    liters: 65,
+    cost: 0.5785,
+  };
+
+  const actual = calculator(data, { toilet: 2, shower: 7 });
+  console.log(expected, actual);
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test('Test #3', (t) => {
+  const expected = {
+    liters: 107.5,
+    cost: 0.95675,
+  };
+
+  const actual = calculator(data, {
+    toilet: 5,
+    shower: 7,
+    'Faucet tap': 3,
+    'Washing machine': 2,
+    Dishwasher: 1,
+  });
+  console.log(expected, actual);
+  t.deepEqual(actual, expected);
   t.end();
 });
