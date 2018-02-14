9500c195f344291233ffaccecb24336a2d82272c
Amir Khoury <amirkhourykh@gmail.com>
2018-02-14 12:49:09 +0200

    Reduced margin in desktop version, Maximized the size of the image in mobile version. Reference #40

--- src/Statistics.jsx
+++ src/Statistics.jsx
@@ -3,27 +3,36 @@ import styled from 'styled-components';
 
 const Container = styled.section`
   text-align: center;
-  margin: 2rem;
+  margin: 1rem 1rem 0 1rem;
   border: #383854 solid 0.2rem;
   border-radius: 0rem 0rem 1rem 1rem;
+
+  @media only screen and (max-width: 420px) {
+    margin: 2rem 0;
+  }
 `;
 
 const ImgContainer = styled.div``;
 
 const Img = styled.img`
-  width: 75%;
+  width: 65%;
 
   @media only screen and (max-width: 700px) {
     width: 85%;
   }
 
   @media only screen and (max-width: 420px) {
-    width: 100;
+    width: 100%;
   }
 `;
 
 const InfoContainer = styled.div`
   padding: : 1rem 0rem 1rem 0rem;
+
+  @media only screen and (max-width: 420px) {
+    padding: 0;
+  }
+
 `;
 
 const Title = styled.h1`
